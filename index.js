cf22c2d38db47912a91bf4042b490b4a65aa27ea
Carlos Vinicius <vinicius.souza5530@gmail.com>
2021-08-20 15:07:54 +0000

    index alt

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
-const lista = ["Mickey", "Donal", "Pateta"];
+const lista = ["Mickey", "Donald", "Pateta"];
 
-app.get("/personagens", function (req, res){
-    res.send("Mickey, Donald and Pateta");
+app.get("/personagens", function (req, res) {
+  res.send(lista);
+});
+
+app.get("/personagens/:id", function (req, res) {
+  const id = req.params.id - 1;
+  const item = lista[id];
+  res.send(item);
 });
 
 app.listen(3000);
